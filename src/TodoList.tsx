bfe8dd380a39919f55b785d13e8e9313cec208ed
Aidar Ibraev <aidaribraev32@gmail.com>
2023-04-13 17:32:54 +0600

    commit - 03

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,45 +1,74 @@
-import React from "react";
+import React, {ChangeEvent,KeyboardEvent, useState} from "react";
 import {FilterValuesType} from "./App";
 
 type TodoListPropsType = {
-    title: string,
-    tasks: TaskType[],
-    removeTask: (taskId: number) => void,
+    title: string
+    tasks: TaskType[]
+    removeTask: (taskId: string) => void
+    addTask: (title: string) => void
     changeFilter: (nextFilter: FilterValuesType) => void
 }
 
 export type TaskType = {
-    id: number,
-    title: string,
+    id: string
+    title: string
     isDone: boolean
 }
 
 const TodoList: React.FC<TodoListPropsType> = (props: TodoListPropsType) => {
+    // const taskTitleInput = useRef<HTMLInputElement>(null);
+    const [title, setTitle] = useState<string>("")
+
+    const setTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+
+    const addTaskHandler = () => {
+        props.addTask(title)
+        setTitle("");
+    }
+
     const tasksListItems: Array<JSX.Element> = props.tasks.map((t) => {
         return (
             <li>
                 <input type="checkbox" checked={t.isDone}/>
-                <span>{ t.title }</span>
-                <button onClick={ () => props.removeTask(t.id) }>X</button>
+                <span>{t.title}</span>
+                <button onClick={() => props.removeTask(t.id)}>X</button>
             </li>
         )
-    })
+    });
 
-    console.log(props);
+    const titleMaxLength = 25;
+    const isTitleLengthTooLong: boolean = title.length > titleMaxLength;
+    const isAddBtnDisabled: boolean = !title.length || title.length > titleMaxLength;
+    const titleMaxLengthWarning = isTitleLengthTooLong
+        ? <div style={{color: "red"}}>Title is too long</div>
+        : null;
+    const handlerCreator = (filter: FilterValuesType) => () => props.changeFilter(filter);
+    const addTaskOnKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => e.key === "Enter" && !isAddBtnDisabled && addTaskHandler();
+
     return (
         <div className="todolist">
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input
+                    // ref={taskTitleInput}
+                    placeholder="Please, enter holder"
+                    value={title}
+                    onChange={setTitleHandler}
+                    onKeyDown={addTaskOnKeyPressHandler}
+                />
+                <button disabled={isAddBtnDisabled}
+                        onClick={addTaskHandler}
+                >+
+                </button>
+                {titleMaxLengthWarning}
             </div>
             <ul>
-                { tasksListItems }
+                {tasksListItems}
             </ul>
             <div>
-                <button onClick={() => props.changeFilter("all")}>All</button>
-                <button onClick={() => props.changeFilter("active")}>Active</button>
-                <button onClick={() => props.changeFilter("completed")}>Completed</button>
+                <button onClick={handlerCreator("all")}>All</button>
+                <button onClick={handlerCreator("active")}>Active</button>
+                <button onClick={handlerCreator("completed")}>Completed</button>
             </div>
         </div>
     )
